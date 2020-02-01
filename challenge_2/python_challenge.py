3ccc7f6997b0b8f3772ec0273e80df6367bb6f68
SenQiao <ilikecookiesandcakes@gmail.com>
2020-02-01 13:32:00 -0500

    Update python_challenge.py
    This is finished challenge 2 python code

--- challenge_2/python_challenge.py
+++ challenge_2/python_challenge.py
@@ -1,6 +1,7 @@
 # Import libraries here!
-
-
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 # First Task
 
 # Please return the key that related to the largest value in the given dictionary
@@ -9,10 +10,36 @@
 
 
 def get_biggest_key(dictionary_obj):
-    # Code goes here!
-    biggest_key = 0
+	# Code goes here!
+	#first item set as biggest
+	biggest_value = list(dictionary_obj.values())[0]; 
+	name_key=list(dictionary_obj.keys())[0]; 
+	#loop through dict
+	for key, value in dictionary_obj.items(): 
+		if value>biggest_value:
+			name_key=key;
+			biggest_value=value;
 
-    return biggest_key
+	return name_key;
+
+	# #if we care about menmory and dict is large
+	# biggest_value = 0; 
+	# name_key='';
+	# index=0;
+	# #loop through dict
+	# for key, value in dictionary_obj.items():
+	# 	if index==0:
+	# 		biggest_value = value; 
+	# 		name_key=key;
+	# 		index=index+1;
+	# 	elif value>biggest_value:
+	# 		name_key=key;
+	# 		biggest_value=value;
+
+	return name_key;
+
+
+
 
 
 # Second Task
@@ -23,7 +50,15 @@ def get_biggest_key(dictionary_obj):
 
 
 def plot_random_gauss(num_values=1000):
-    # Code goes here!
+	# Code goes here!
+	mean = random.randint(-1000,1000); std = random.randint(1,4);
+	x = np.arange(mean-5,mean+5,10/num_values)
+	f = np.exp(-np.square(x-mean)/2*np.square(std))/(np.sqrt(2*np.pi*np.square(std)))
+	plt.plot(x,f);
+	plt.show();
 
-    return None
 
+# dictionary={'first': 2, 'second': 908, 'third': -5, 'fourth': 2**10};
+# print(get_biggest_key(dictionary));
+
+# plot_random_gauss();
